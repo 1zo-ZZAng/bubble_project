58cc3a6a1136d1e1f8bf6fb77dfe93c75538c4c7
0hzL <arin980921@naver.com>
2023-05-09 17:29:45 +0900

    test

---
+++ src/main/java/com/example/bubble_project/BubbleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.bubble_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubbleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubbleProjectApplication.class, args);
+	}
+
+}
