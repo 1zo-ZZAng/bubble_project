309cc10a00b37f240dfceb9a859c3949807b0f39
탁승은 <xkrtmddms@gmail.com>
2023-05-17 16:52:07 +0900

    관리자, 예약, 기기, 세탁업체 엔티티

--- src/main/java/com/example/entity/Washing.java
+++ src/main/java/com/example/entity/Washing.java
@@ -1,6 +1,8 @@
 package com.example.entity;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
