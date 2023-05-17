b1d0f36f30ac53537f67c4d26c13789900a9edb3
탁승은 <xkrtmddms@gmail.com>
2023-05-18 08:42:34 +0900

    security config 수정 중

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -11,8 +12,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.example.handler.CustomLogoutSuccessHandler;
 import com.example.handler.CustomLoginSuccessHandler;
+import com.example.handler.CustomLogoutSuccessHandler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,10 +24,84 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    // 고객 테이블 (1)
+    // 업체 테이블 (2)
+    // 관리자 테이블 (3)
+    
     final UserDetailsService userDetailsService; //구현한 서비스 SecurityServiceImpl.java
 
+    
+
+    //고객
+    @Bean   // 객체를 생성함. (자동으로 호출됨.)
+    @Order(value = 1) // 순서를 먼저 설정 //가장먼저 처리
+    public SecurityFilterChain filterChain1(HttpSecurity http) throws Exception {
+        log.info("SecurityConfig => {}", "start filter chain2");
+
+        // 127.0.0.1:9090/ROOT/student2/login.do
+        // 127.0.0.1:9090/ROOT/student2/loginaction.do
+        // 127.0.0.1:9090/ROOT/student2/logout.do
+        // 위의 두개의 주소만 필터함.
+        http.antMatcher("/student2/login.do")
+            .antMatcher("/student2/loginaction.do") 
+            .authorizeRequests()
+            .anyRequest().authenticated().and();
+
+        // 로그인 처리
+        http.formLogin()
+            .loginPage("/student2/login.do")
+            .loginProcessingUrl("/student2/loginaction.do")
+            .usernameParameter("id") //login.html의 email name값
+            .passwordParameter("password") // login.html의 password name값
+            .defaultSuccessUrl("/student2/home.do")
+            .permitAll();
+
+        //로그아웃 처리는 한번만 하면 됨 밑에서 굳이 여기서 다 안해도 됨
+
+
+        http.userDetailsService(student2TableService);
+        return http.build();
+    }
+
+
+    //업체
+    @Bean   // 객체를 생성함. (자동으로 호출됨.)
+    @Order(value = 2) // 순서를 먼저 설정 //가장먼저 처리
+    public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
+        log.info("SecurityConfig => {}", "start filter chain2");
+
+        // 127.0.0.1:9090/ROOT/student2/login.do
+        // 127.0.0.1:9090/ROOT/student2/loginaction.do
+        // 127.0.0.1:9090/ROOT/student2/logout.do
+        // 위의 두개의 주소만 필터함.
+        http.antMatcher("/student2/login.do")
+            .antMatcher("/student2/loginaction.do") 
+            .authorizeRequests()
+            .anyRequest().authenticated().and();
+
+        // 로그인 처리
+        http.formLogin()
+            .loginPage("/student2/login.do")
+            .loginProcessingUrl("/student2/loginaction.do")
+            .usernameParameter("id") //login.html의 email name값
+            .passwordParameter("password") // login.html의 password name값
+            .defaultSuccessUrl("/student2/home.do")
+            .permitAll();
+
+        //로그아웃 처리는 한번만 하면 됨 밑에서 굳이 여기서 다 안해도 됨
+
+
+        http.userDetailsService(student2TableService);
+        return http.build();
+    }
+
+
+
+
+    //관리자
     @Bean //객체 생성
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    @Order(value=3)
+    public SecurityFilterChain filterChain3(HttpSecurity http) throws Exception{
         log.info("SecurityConfig => {}","start filter chain");
 
         //권한설정
