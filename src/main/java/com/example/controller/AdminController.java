c7b17526cbdd9ade2df42f12ac59871ca90bfd57
0hzL <arin980921@naver.com>
2023-05-18 15:47:09 +0900

    230518

---
+++ src/main/java/com/example/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping(value = "/admin")
+@RequiredArgsConstructor
+public class AdminController {
+    
+}
