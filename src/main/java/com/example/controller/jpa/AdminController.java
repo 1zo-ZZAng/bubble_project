d070085cc28a387c0f1b60154e10f3923fcddfe8
탁승은 <xkrtmddms@gmail.com>
2023-05-18 17:39:48 +0900

    filter 다시

--- src/main/java/com/example/controller/jpa/AdminController.java
+++ src/main/java/com/example/controller/jpa/AdminController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping(value = "/admin")
