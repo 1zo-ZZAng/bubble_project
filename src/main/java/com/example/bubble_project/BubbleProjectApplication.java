32f60d3993df8e7b94b2f283aee6a1bd2b44fec4
yeofriendship <yeofriendship@naver.com>
2023-05-18 16:41:49 +0900

    ㅁ

--- src/main/java/com/example/bubble_project/BubbleProjectApplication.java
+++ src/main/java/com/example/bubble_project/BubbleProjectApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+<<<<<<< HEAD
 @PropertySource(value = {"classpath:global.properties"}) // 직접 만든 환경설정파일 위치
 // @MapperScan(basePackages = {"com.example.mapper"}) // 매퍼 위치 설정
 @ComponentScan(basePackages = {"com.example.controller", 
@@ -21,6 +22,17 @@ import org.springframework.context.annotation.PropertySource;
 					}) // 컨트롤러, 서비스 위치, 시큐리티환경, 레스트컨트롤러 설정
 @EntityScan(basePackages = {"com.example.entity"}) // 엔티티 위치
 // @EnableJpaRepositories(basePackages = {"com.example.repository"}) // 저장소 위치
+=======
+@PropertySource(value= {"classpath:global.properties"}) // 직접만든 환경설정파일 위치
+@MapperScan(basePackages = {"com.example.mapper"}) // 매퍼 위치 설정
+@ComponentScan(basePackages = {"com.example.controller",
+							   "com.example.controller.jpa", 
+							   "com.example.controller.mybatis", 
+							   "com.example.service", 
+							   "com.example.config", 
+							   "com.example.restcontroller", 
+							   "com.example.filter"}) //컨트롤러, 서비스 위치, 시큐리티환결설정, 레스트컨트롤러, 필터
+>>>>>>> 여우정
 
 public class BubbleProjectApplication {
 
