2ea4ef3ceb63e50c63c346c63a8764c995d95168
0hzL <arin980921@naver.com>
2023-05-18 17:25:09 +0900

    css 수정

--- src/main/java/com/example/controller/jpa/AdminController.java
+++ src/main/java/com/example/controller/jpa/AdminController.java
@@ -1,14 +1,65 @@
 package com.example.controller.jpa;
 
+
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.entity.Admin;
+import com.example.repository.AdminRepository;
+
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Data
+@Slf4j
 @Controller
 @RequestMapping(value = "/admin")
 @RequiredArgsConstructor
 public class AdminController {
     
+    final String format = "AdminController => {}";
+    final AdminRepository aRepository;
+    BCryptPasswordEncoder bcpe = new BCryptPasswordEncoder();
+
+    @GetMapping(value = "/join.bubble")
+    public String joinGET() {
+        return "/admin/join"; // templates/admin/join.html
+    }
+
+
+    @PostMapping(value = "/join.bubble")
+    public String joinPOST(@ModelAttribute Admin admin) {
+        try{
+            admin.setPassword(bcpe.encode(admin.getPassword()));
+            admin.setRole("ADMIN");
+            log.info("{}", admin.toString());
+            aRepository.save(admin);
+            return "redirect:/admin/join.bubble";
+        }
+        catch( Exception e){
+            e.printStackTrace();
+            return "redirect:/admin/join.bubble";
+        }
+       
+
+    }
+
+
+
+    @GetMapping(value = "/login.bubble")
+    public String loginGET(){
+        try {
+            return "/admin/login";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/home.do";
+        }
+    }
+
 }
