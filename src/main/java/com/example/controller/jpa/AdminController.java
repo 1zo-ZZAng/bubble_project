be7678f96505f43388b95502be76afa8b57ac9b0
0hzL <arin980921@naver.com>
2023-05-19 14:28:25 +0900

    230519

--- src/main/java/com/example/controller/jpa/AdminController.java
+++ src/main/java/com/example/controller/jpa/AdminController.java
@@ -5,8 +5,11 @@ package com.example.controller.jpa;
 import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +33,7 @@ public class AdminController {
     final AdminRepository aRepository;
     BCryptPasswordEncoder bcpe = new BCryptPasswordEncoder();
 
+    //127.0.0.1:8282/bubble_bumul/admin/join.bubble
     @GetMapping(value = "/join.bubble")
     public String joinGET() {
         return "/admin/join"; // templates/admin/join.html
@@ -54,7 +58,7 @@ public class AdminController {
     }
 
 
-
+//127.0.0.1:8282/bubble_bumul/admin/login.bubble
     @GetMapping(value = "/login.bubble")
     public String loginGET(){
         try {
@@ -65,22 +69,47 @@ public class AdminController {
         }
     }
 
-    // @PostMapping(value = "/loginaction.bubble")
-    // public String loginActionPOST(@ModelAttribute Admin admin){
-    //     try {
-    //         log.info("loginAction => {}", admin.toString());
+    @PostMapping(value = "/loginaction.bubble")
+    public String loginActionPOST(@ModelAttribute Admin admin){
+        try {
+            log.info("loginAction => {}", admin.toString());
 
-    //         Admin obj = aRepository.findById(admin.getId()).orElse(null);
+            Admin obj = aRepository.findById(admin.getId()).orElse(null);
 
-    //         //암호비교
-    //         if(bcpe.matches(obj.getPassword(), admin.getPassword())){
-    //            aRepository.save(obj);
-    //            log.info(format, obj);
-    //         }
-    //         return "redirect:/admin/home.bubble";
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //         return "redirect:/admin/loigin.bubble";
+            //암호비교
+            if(bcpe.matches(obj.getPassword(), admin.getPassword())){
+               aRepository.save(obj);
+               log.info(format, obj);
+            }
+            return "redirect:/admin/home.bubble";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/admin/login.bubble";
+        }
+    }
+
+        //임포트 shift + alt + o
+     //127.0.0.1:8282/bubble_bumul/admin/home.bubble
+     @GetMapping(value = "/home.bubble")
+    public String homeGET(@AuthenticationPrincipal User user, Model model) {
+        try {
+            model.addAttribute("user", user);
+            return "/admin/home";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/login.bubble";
+        }
+    }
+
+
+    // @GetMapping(value = "business_num.bubble")
+    // public String business_numGET(@ModelAttribute ){
+    //     try{
+
+    //     }
+    //     catch(Exception e){
+
+    //         return "redirect:/business_num.bubble"
     //     }
     // }
 
