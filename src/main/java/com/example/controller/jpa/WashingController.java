1f6e15e7e25647139754417625a6ea1fe18f774e
탁승은 <xkrtmddms@gmail.com>
2023-05-22 13:11:24 +0900

    수정

--- src/main/java/com/example/controller/jpa/WashingController.java
+++ src/main/java/com/example/controller/jpa/WashingController.java
@@ -1,10 +1,17 @@
 package com.example.controller.jpa;
 
+import java.math.BigInteger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,15 +32,18 @@ import lombok.extern.slf4j.Slf4j;
 
 
 
+
 @Controller
 @RequestMapping(value = "/washing")
 @RequiredArgsConstructor
 @Slf4j
 public class WashingController {
 
+    //업체
     final WashingRepository wRepository;
     final WashingService wService;
 
+    //기기
     final MachineRepository mRepository;
 
     final HttpSession httpSession;
@@ -124,7 +134,9 @@ public class WashingController {
     public String updateGET(@RequestParam(name = "id") String id, Model model, @ModelAttribute Washing washing) {
         try {
 
-            Washing obj = wRepository.findById(id).orElse(null);
+            log.info("아이디 => {}", id.toString());
+
+            Washing obj = wRepository.findById(id).orElse(null);            
 
             model.addAttribute("washing", obj);
 
@@ -140,15 +152,20 @@ public class WashingController {
     public String updatePOST( @AuthenticationPrincipal User user, @ModelAttribute Washing washing) {
         
         try {
+
+            log.info("정보수정 할 아이디 => {}", user.getUsername().toString());
             
             //기존 데이터 읽어오기
             Washing obj = wRepository.findById(user.getUsername()).orElse(null);
+
             
             obj.setPhone(washing.getPhone());
             obj.setCeo(washing.getCeo()); //대표자명
             obj.setEmail(washing.getEmail());
             obj.setName(washing.getName()); //업체명
             obj.setAddress(washing.getAddress());
+
+            log.info("수정 정보 => {}", obj.toString());
 
             //변경항목 저장
             wRepository.save(obj);
@@ -199,16 +216,24 @@ public class WashingController {
 
                 //변경사항 저장
                 wRepository.save(obj);
+
+                model.addAttribute("msg", "비밀번호가 변경되었습니다");
+                model.addAttribute("url","/bubble_bumul/washing/pwupdate.bubble?id=" + obj.getId());
+
+                return "message";
+
             }
 
-            model.addAttribute("msg", "비밀번호가 변경되었습니다");
-            model.addAttribute("url","/bubble_bumul/washing/pwupdate.bubble?id="+washing.getId());
+            model.addAttribute("msg", "비밀번호를 다시 확인해주세요");
+            model.addAttribute("url","/bubble_bumul/washing/pwupdate.bubble?id=" + obj.getId());
 
-
-            // return "redirect:/washing/pwupdate.bubble?id="+washing.getId();
             return "message";
+            
+            
 
         } catch (Exception e) {
+
+
             e.printStackTrace();
             return "redirect:/washing/pwupdate.bubble?id="+washing.getId();
         }
@@ -226,6 +251,54 @@ public class WashingController {
             return "redirect:/washing/home.bubble";
         }
     }
+
+    @PostMapping(value="/delete.bubble")
+    public String deletePOST(@RequestParam(name = "id") String id, @RequestParam(name = "password") String password, @AuthenticationPrincipal User user, @ModelAttribute Washing washing , Model model, HttpServletRequest request, HttpServletResponse response) {
+        try {
+
+            //기존데이터 읽어오기
+            Washing obj = wRepository.findById(user.getUsername()).orElse(null);
+
+            //암호 비교
+            if(bcpe.matches(washing.getPassword(), obj.getPassword())){
+
+                obj.setPassword(null);
+                obj.setWnumber(null);
+                obj.setEmail(null);
+                obj.setName("탈퇴업체");
+                obj.setAddress(null);
+                obj.setPhone(null);
+                obj.setCeo(null);
+                obj.setRole(null);
+                obj.setChk(BigInteger.valueOf(0));
+
+                //변경항목에 저장
+                wRepository.save(obj);
+
+                Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+                if (auth != null) {
+                    new SecurityContextLogoutHandler().logout(request, response, auth);
+                }
+
+                //탈퇴 후 메시지
+                model.addAttribute("msg", "탈퇴가 완료되었습니다");
+                model.addAttribute("url","/bubble_bumul/home.bubble" );
+
+                return "message";
+
+            }
+
+            model.addAttribute("msg", "탈퇴 실패!");
+            model.addAttribute("url","/bubble_bumul/washing/mypage.bubble?id=" + washing.getId());
+
+            return "message";
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "redirect:/washing/home.bubble";
+        }
+    }
+    
     
     
 
