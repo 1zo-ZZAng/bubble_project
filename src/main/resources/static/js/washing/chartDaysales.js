888fd7a877edb562b402a317f6adea92c87a5ace
탁승은 <xkrtmddms@gmail.com>
2023-05-29 15:58:02 +0900

    수정

--- src/main/resources/static/js/washing/chartDaysales.js
+++ src/main/resources/static/js/washing/chartDaysales.js
@@ -1,57 +1,85 @@
-// Set new default font family and font color to mimic Bootstrap's default styling
-// Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontFamily = '"IBM Plex Sans KR", sans-serif';
-
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
-// Area Chart Example
 
-var ctx = document.getElementById("daySalesChart");
-var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["3월 1일", "3월 2일", "3월 3일", "3월 4일", "3월 5일", "3월 6일", "3월 7일", "3월 8일", "3월 9일", "3월 10일", "3월 11일", "3월 12일", "3월 13일"],
-    datasets: [{
-      label: "Sessions",
-      lineTension: 0.3,
-      backgroundColor: "rgba(2,117,217,0.2)",
-      borderColor: "rgba(2,117,217,1)",
-      pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,217,1)",
-      pointBorderColor: "rgba(255,255,255,0.8)",
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,217,1)",
-      pointHitRadius: 50,
-      pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
+var chartArea = {
+  labels : [],
+  dataSets : [],
+  render : function() {
+    new Chart($("#myAreaChart"), {
+      type: 'line',
+      data: {
+        labels: chartArea.labels,
+        datasets: [{
+          label: "매출액",
+          lineTension: 0.3,
+          backgroundColor: "rgba(2,117,216,0.2)",
+          borderColor: "rgba(2,117,216,1)",
+          pointRadius: 5,
+          pointBackgroundColor: "rgba(2,117,216,1)",
+          pointBorderColor: "rgba(255,255,255,0.8)",
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(2,117,216,1)",
+          pointHitRadius: 50,
+          pointBorderWidth: 2,
+          data: chartArea.dataSets,
+        }],
+      },
+      options: {
+        responsive : true,
+        scales: {
+          xAxes: [{
+            time: {
+              unit: 'date'
+            },
+            gridLines: {
+              display: false
+            },
+            ticks: {
+              maxTicksLimit: 7
+            }
+          }],
+          yAxes: [{
+/*          y값에 따라 최소, 최대치 표시를 자동으로 설정하기 위해 주석처리
+              ticks: {
+              min: 0,
+              max: 40000,
+              maxTicksLimit: 5
+            },*/
+            gridLines: {
+              color: "rgba(0, 0, 0, .125)",
+            }
+          }],
         },
-        gridLines: {
+        legend: {
           display: false
-        },
-        ticks: {
-          maxTicksLimit: 7
         }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 40000,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          color: "rgba(0, 0, 0, .125)",
-        }
-      }],
-    },
-    legend: {
-      display: false
-    }
+      }
+    });
+  },
+  showData : function(){
+    labels = [];
+    dataSets= [];
+    $.ajax({
+      type : 'GET',
+      url : 'daysales.bubble',
+      contentType: 'application/json',
+      //dataType 정의
+      dataType: 'json',
+      //요청결과가 성공일 경우
+      success : function(data) {
+        //console.log(data);
+        $.each(data, function(index,obj){
+          chartArea.labels.push(obj.DATE);
+          chartArea.dataSets.push(obj.DAYSALES);
+        });
+        chartArea.render();
+      },
+      //요청결과가 실패일 경우
+      error : function(xhr, status, error){
+      }
+    });
   }
-});
+};
+
+chartArea.showData();
