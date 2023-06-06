7a34834008b6dea593e8936537beb3e1a39503d4
탁승은 <xkrtmddms@gmail.com>
2023-06-06 16:09:00 +0900

    스케쥴러 service

--- src/main/java/com/example/service/mybatis/SchedulerMyBatisServie.java
+++ src/main/java/com/example/service/mybatis/SchedulerMyBatisServie.java
@@ -9,9 +9,10 @@ import com.example.dto.Reserve;
 @Service
 public interface SchedulerMyBatisServie {
 
-    //스케쥴러
+    //스케쥴러 - 예약 조회 (reserve)
     public List<Reserve> selectReserveListSch();
 
+    //스케쥴러 - 예약 업데이트 (reservation)
     public int updateReserveState(String rvno);
 
 
