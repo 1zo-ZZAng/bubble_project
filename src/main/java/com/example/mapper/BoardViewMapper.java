0dcae02c46453ab87265e668065a90a93731e9e9
탁승은 <xkrtmddms@gmail.com>
2023-06-08 00:20:37 +0900

    수정

--- src/main/java/com/example/mapper/BoardViewMapper.java
+++ src/main/java/com/example/mapper/BoardViewMapper.java
@@ -12,25 +12,25 @@ public interface BoardViewMapper {
 
 
     //전체 조회
-    @Select({" SELECT * FROM boardview"})
+    @Select({" SELECT * FROM boardview ORDER BY no DESC "})
     public List<BoardView> selectBoardView();
 
     /* 카테고리 별 조회 */
 
     //공지사항
-    @Select({" SELECT * FROM boardview WHERE code=1"})
+    @Select({" SELECT * FROM boardview WHERE code=1 ORDER BY no DESC "})
     public List<BoardView> selectBoardViewNotice();
 
     //분실물
-    @Select({" SELECT * FROM boardview WHERE code=2"})
+    @Select({" SELECT * FROM boardview WHERE code=2 ORDER BY no DESC "})
     public List<BoardView> selectBoardViewLost();
 
     //습득물
-    @Select({" SELECT * FROM boardview WHERE code=3"})
+    @Select({" SELECT * FROM boardview WHERE code=3 ORDER BY no DESC "})
     public List<BoardView> selectBoardViewGet();
 
     //자유게시판
-    @Select({" SELECT * FROM boardview WHERE code=4"})
+    @Select({" SELECT * FROM boardview WHERE code=4 ORDER BY no DESC "})
     public List<BoardView> selectBoardViewGeneral();
     
 }
